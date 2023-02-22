76c7b18b392448e660b7d829b073c7e19412c47b
Olowosuyi Temitope <Temitopeabiodun685@gmail.com>
2023-02-22 00:48:46 +0000

    main.c to compile our program

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - the entry point to our program
+ * @argv: argument vector
+ * @argc: argument counter
+ *
+ * Return: 0 (success)
+ */
+
+int main(int argc, char **argv)
+{
+	simple_shell(argv[0]);
+	return (0);
+}
